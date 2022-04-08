e164ca50c7a8d7b44863e4e6a1aff433f5efb15a
Belarif <b.ocine@live.fr>
2022-04-08 13:39:37 +0200

    BM1: creating code that create users

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $hasher;
+
+    public const ROLE_USER = 'ROLE_USER';
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+
+        $users = [
+            ['email' => 'b.ocine@live.fr', 'password' => 'user1'],
+            ['email' => 'exemple@gmail.com', 'password' => 'user2']
+        ];
+
+        $user = new User();
+
+        foreach ($users as $addUser){
+            $user->setEmail($addUser['email']);
+            $user->setPassword($this->hasher->hashPassword($user,$addUser['password']));
+            $user->setRoles([self::ROLE_USER]);
+
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+    }
+}
