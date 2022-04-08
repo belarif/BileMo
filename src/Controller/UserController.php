8f2c08d5f41f25a25f2cc6132390aa7058faa86f
Belarif <b.ocine@live.fr>
2022-04-08 14:29:42 +0200

    BM1: creating controller and function index to test JWT authentication token

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/api/users", name="app_user")
+     */
+    public function index(): Response
+    {
+        return $this->json([
+            'message' => 'Vous vous etes authentifié avec succès'
+        ]);
+    }
+}
