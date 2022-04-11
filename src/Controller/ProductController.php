e2879b0085f381ed64c944ff3b3b7d5a7780e7c2
Belarif <b.ocine@live.fr>
2022-04-11 17:57:59 +0200

    BM2: creating route and function that allows to add a product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,20 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
-    /**
-     * @Route("/products", name="api_create_product")
-     */
-    public function createProduct(): Response
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $this->em = $entityManager;
+    }
+
+    /**
+     * @Route("/api/products", name="api_create_product", methods={"POST"})
+     */
+    public function createProduct(Request $request, SerializerInterface $serializer)
+    {
+        $data = $request->getContent();
+        $product = $serializer->deserialize($data,Product::class,'json');
+
+        $this->em->persist($product);
+        $this->em->flush();
+
+        return new Response('Le produit a été ajouté avec succès',Response::HTTP_CREATED);
     }
 }
