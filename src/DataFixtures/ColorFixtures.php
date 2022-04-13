84969d4907b9d80de53e6d511efeda0df21e7e31
Belarif <b.ocine@live.fr>
2022-04-13 15:13:31 +0200

    BM2: creating ColorFixtures to link to product

---
+++ src/DataFixtures/ColorFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Color;
+use Doctrine\Persistence\ObjectManager;
+
+class ColorFixtures extends \Doctrine\Bundle\FixturesBundle\Fixture
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $colors = ['Blanc','Bleu','Doré','Gris argent','Noir','Rose'];
+
+        foreach($colors as $addColor) {
+            $color = new Color();
+
+            $color->setName($addColor);
+
+            $manager->persist($color);
+            $manager->flush();
+        }
+    }
+}
