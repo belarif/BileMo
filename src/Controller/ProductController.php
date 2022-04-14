8600d212bfd19d149371173d0838396f44eb0e40
Belarif <b.ocine@live.fr>
2022-04-14 17:07:59 +0200

    BM2: persisting object in ProductRepository and removing it persistance in ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,19 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     private $em;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    private $productRepository;
+
+    public function __construct(EntityManagerInterface $entityManager, ProductRepository $productRepository)
     {
         $this->em = $entityManager;
+        $this->productRepository = $productRepository;
     }
 
     /**
@@ -23,14 +28,15 @@ class ProductController extends AbstractController
      * @param Request $request
      * @param SerializerInterface $serializer
      * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function createProduct(Request $request, SerializerInterface $serializer):JsonResponse
     {
         $data = $request->getContent();
         $product = $serializer->deserialize($data,Product::class,'json');
 
-        $this->em->persist($product);
-        $this->em->flush();
+        $this->productRepository->add($product);
 
         return new JsonResponse('le produit a été créé avec succès','201');
     }
