e54806975ae3a8532a974118cf2d4bbd239d9190
Belarif <b.ocine@live.fr>
2022-04-19 15:23:51 +0200

    BM2: creating Color, Country, Memory, Product and User Objects Transfert Data (DTOs)

---
+++ src/Entity/DTO/ColorDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity\DTO;
+
+class ColorDTO
+{
+    public int $id;
+
+    public string $name;
+
+}
---
+++ src/Entity/DTO/MemoryDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Entity\DTO;
+
+class MemoryDTO
+{
+    public int $id;
+
+    public string $memoryCapacity;
+
+}
