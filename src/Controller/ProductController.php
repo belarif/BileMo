5d8363844b2c3b0b03e53186c54c6392f665024e
Belarif <b.ocine@live.fr>
2022-04-19 17:08:14 +0200

    BM2: creating productManagement in order to refactor ProductController

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,91 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\DTO\ProductDTO;
-use App\Entity\Product;
-use App\Repository\BrandRepository;
-use App\Repository\ColorRepository;
-use App\Repository\CountryRepository;
-use App\Repository\MemoryRepository;
-use App\Repository\ProductRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\productManagement;
 
 class ProductController extends AbstractController
 {
-    private $em;
-
-    private $productRepository;
-
-    private $brandRepository;
-
-    private $memoryRepository;
-
-    private $countryRepository;
-
-    private $userRepository;
-
-    private $colorRepository;
-
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        ProductRepository $productRepository,
-        BrandRepository $brandRepository,
-        MemoryRepository $memoryRepository,
-        CountryRepository $countryRepository,
-        UserRepository $userRepository,
-        ColorRepository $colorRepository
-
-    )
-    {
-        $this->em = $entityManager;
-        $this->productRepository = $productRepository;
-        $this->brandRepository = $brandRepository;
-        $this->memoryRepository = $memoryRepository;
-        $this->countryRepository = $countryRepository;
-        $this->userRepository = $userRepository;
-        $this->colorRepository = $colorRepository;
-    }
-
     /**
      * @Route("/products", name="api_create_product", methods={"POST"})
      * @param Request $request
      * @param SerializerInterface $serializer
+     * @param productManagement $productManagement
      * @return JsonResponse
-     * @throws \Doctrine\ORM\ORMException
-     * @throws \Doctrine\ORM\OptimisticLockException
      */
-    public function createProduct(Request $request, SerializerInterface $serializer):JsonResponse
+    public function create(Request $request, SerializerInterface $serializer, productManagement $productManagement):JsonResponse
     {
-
         /**
          * @var ProductDTO $productDTO
          */
         $productDTO = $serializer->deserialize($request->getContent(), ProductDTO::class, 'json');
-
-        $brand = $this->brandRepository->findOneBy(['id' => $productDTO->brand->id]);
-        $memory = $this->memoryRepository->findOneBy(['id' => $productDTO->memory->id]);
-        $country = $this->countryRepository->findOneBy(['id' => $productDTO->country->id]);
-        $user = $this->userRepository->findOneBy(['id' => $productDTO->user->id]);
-
-        $product = new Product();
-        $product->setName($productDTO->name);
-        $product->setDescription($productDTO->description);
-        $product->setBrand($brand);
-        $product->setMemory($memory);
-        $product->setCountry($country);
-        $product->setUser($user);
-
-        foreach ($productDTO->getColors() as $color) {
-            $product->addColor($this->colorRepository->findOneBy(['id' => $color->id]));
-        }
-
-        $this->productRepository->add($product);
+        $productManagement->createProduct($productDTO);
 
         return new JsonResponse('le produit a été créé avec succès','201');
     }
 }
+
---
+++ src/Service/ProductManagement.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\DTO\ProductDTO;
+use App\Entity\Product;
+use App\Repository\BrandRepository;
+use App\Repository\ColorRepository;
+use App\Repository\CountryRepository;
+use App\Repository\MemoryRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\OptimisticLockException;
+
+class productManagement
+{
+    protected EntityManagerInterface $em;
+
+    private ProductRepository $productRepository;
+
+    private BrandRepository $brandRepository;
+
+    private MemoryRepository $memoryRepository;
+
+    private CountryRepository $countryRepository;
+
+    private UserRepository $userRepository;
+
+    private ColorRepository $colorRepository;
+
+    public function __construct(
+        EntityManagerInterface $em,
+        ProductRepository $productRepository,
+        BrandRepository $brandRepository,
+        MemoryRepository $memoryRepository,
+        CountryRepository $countryRepository,
+        UserRepository $userRepository,
+        ColorRepository $colorRepository
+    )
+    {
+        $this->em = $em;
+        $this->productRepository = $productRepository;
+        $this->brandRepository = $brandRepository;
+        $this->memoryRepository = $memoryRepository;
+        $this->countryRepository = $countryRepository;
+        $this->userRepository = $userRepository;
+        $this->colorRepository = $colorRepository;
+    }
+
+    /**
+     * @param ProductDTO $productDTO
+     * @throws OptimisticLockException
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function createProduct(ProductDTO $productDTO)
+    {
+        $brand = $this->brandRepository->findOneBy(['id' => $productDTO->brand->id]);
+        $memory = $this->memoryRepository->findOneBy(['id' => $productDTO->memory->id]);
+        $country = $this->countryRepository->findOneBy(['id' => $productDTO->country->id]);
+        $user = $this->userRepository->findOneBy(['id' => $productDTO->user->id]);
+
+        $product = new Product();
+        $product->setName($productDTO->name);
+        $product->setDescription($productDTO->description);
+        $product->setBrand($brand);
+        $product->setMemory($memory);
+        $product->setCountry($country);
+        $product->setUser($user);
+
+        foreach ($productDTO->getColors() as $color) {
+            $product->addColor($this->colorRepository->findOneBy(['id' => $color->id]));
+        }
+
+        $this->productRepository->add($product);
+    }
+}
