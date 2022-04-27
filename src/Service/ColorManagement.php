ec019c509fbdcd289ef5a3a8f3635c6620278004
Belarif <b.ocine@live.fr>
2022-04-27 18:39:17 +0200

    BM27: creating api that allow to create color

---
+++ src/Service/ColorManagement.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Color;
+use App\Entity\DTO\ColorDTO;
+use App\Repository\ColorRepository;
+
+class ColorManagement
+{
+    private ColorRepository $colorRepository;
+
+    /**
+     * @param ColorRepository $colorRepository
+     */
+    public function __construct(ColorRepository $colorRepository)
+    {
+        $this->colorRepository = $colorRepository;
+    }
+
+    /**
+     * @param ColorDTO $colorDTO
+     * @return void
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function createColor(ColorDTO $colorDTO)
+    {
+        $color = new Color();
+        $color->setName($colorDTO->name);
+
+        $this->colorRepository->add($color);
+    }
+}
