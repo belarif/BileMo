4a54f6ca938496b13023bcf6956d33e8c037e691
Belarif <b.ocine@live.fr>
2022-04-27 14:47:58 +0200

    creating api that allow to display brands list

--- src/Controller/BrandController.php
+++ src/Controller/BrandController.php
@@ -29,4 +29,15 @@ class BrandController extends AbstractController
 
         return $this->json('La marque a été ajouté avec succès',200,['Content-Type' => 'text/plain']);
     }
+
+    /**
+     * @Route("", name="brands_list", methods={"GET"})
+     * @param BrandManagement $brandManagement
+     * @return JsonResponse
+     */
+    public function list(BrandManagement $brandManagement): JsonResponse
+    {
+        return $this->json($brandManagement->brandsList(),'200',['Content-Type' => 'application/json']);
+    }
+
 }
