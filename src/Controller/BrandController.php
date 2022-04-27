bf6db1d9fa5e67132b41a857d9d7beb116c8e84e
Belarif <b.ocine@live.fr>
2022-04-27 14:27:13 +0200

    BM17: creating api that allow to create new marque

---
+++ src/Controller/BrandController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\DTO\BrandDTO;
+use App\Service\BrandManagement;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/brands", "api_")
+ */
+class BrandController extends AbstractController
+{
+    /**
+     * @Route("", name="create_brand", methods={"POST"})
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param BrandManagement $brandManagement
+     * @return JsonResponse
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function create(Request $request, SerializerInterface $serializer, BrandManagement $brandManagement): JsonResponse
+    {
+        $brandDTO = $serializer->deserialize($request->getContent(),BrandDTO::class,'json');
+
+        $brandManagement->createBrand($brandDTO);
+
+        return $this->json('La marque a été ajouté avec succès');
+    }
+}
---
+++ src/Service/BrandManagement.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Brand;
+use App\Repository\BrandRepository;
+
+class BrandManagement
+{
+    private BrandRepository $brandRepository;
+
+    public function __construct(BrandRepository $brandRepository)
+    {
+        $this->brandRepository = $brandRepository;
+    }
+
+    /**
+     * @param $brandDTO
+     * @return void
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function createBrand($brandDTO)
+    {
+        $brand = new Brand();
+        $brand->setName($brandDTO->name);
+
+        $this->brandRepository->add($brand);
+    }
+}
