b98a758e56d5f6a4ca4e4aa9be5ee2a6e1786676
Belarif <b.ocine@live.fr>
2022-05-06 11:46:19 +0200

    BM39: urenaming classes and methods from User to UserOfCustomer

--- src/Controller/UserOfCustomerController.php
+++ src/Controller/UserOfCustomerController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Customer;
 use App\Entity\DTO\UserDTO;
 use App\Entity\User;
-use App\Service\UserManagement;
+use App\Service\UserOfCustomerManagement;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,20 +16,20 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 /**
  * @Route("/customers/{customer_id}/users", name="api_", requirements={"customer_id"="\d+"})
  */
-class UserController extends AbstractController
+class UserOfCustomerController extends AbstractController
 {
     /**
      * @Route("", name="create_user", methods={"POST"})
      *
      * @Entity("customer", expr="repository.getCustomer(customer_id)")
      */
-    public function create(Request $request, SerializerInterface $serializer, UserManagement $userManagement, Customer $customer): JsonResponse
+    public function create(Request $request, SerializerInterface $serializer, UserOfCustomerManagement $userManagement, Customer $customer): JsonResponse
     {
         /**
          * @var UserDTO $userDTO
          */
         $userDTO = $serializer->deserialize($request->getContent(), UserDTO::class, 'json');
-        $userManagement->createUser($userDTO, $customer);
+        $userManagement->createUserOfCustomer($userDTO, $customer);
 
         return $this->json('L\'utilisateur a été créé avec succès',200,['Content-Type' => 'text/plain']);
     }
@@ -39,7 +39,7 @@ class UserController extends AbstractController
      *
      * @Entity("customer", expr="repository.getCustomer(customer_id)")
      */
-    public function list(UserManagement $userManagement, Customer $customer): JsonResponse
+    public function list(UserOfCustomerManagement $userManagement, Customer $customer): JsonResponse
     {
         return $this->json($userManagement->usersOfCustomer($customer),200,['Content-Type' => 'application/json']);
     }
@@ -50,9 +50,9 @@ class UserController extends AbstractController
      * @Entity("customer", expr="repository.getCustomer(customer_id)")
      * @Entity("user", expr="repository.getUser(user_id)")
      */
-    public function show(Request $request, Customer $customer, UserManagement $userManagement, User $user): JsonResponse
+    public function show(Request $request, Customer $customer, UserOfCustomerManagement $userManagement, User $user): JsonResponse
     {
-        return $this->json($userManagement->showUser($request->get('user_id'),$customer),200,['Content-Type' => 'application/json']);
+        return $this->json($userManagement->showUserOfCustomer($request->get('user_id'),$customer),200,['Content-Type' => 'application/json']);
     }
 
     /**
@@ -61,10 +61,10 @@ class UserController extends AbstractController
      * @Entity("customer", expr="repository.getCustomer(customer_id)")
      * @Entity("user", expr="repository.getUser(user_id)")
      */
-    public function update(Request $request, SerializerInterface $serializer, UserManagement $userManagement, User $user, Customer $customer): JsonResponse
+    public function update(Request $request, SerializerInterface $serializer, UserOfCustomerManagement $userManagement, User $user, Customer $customer): JsonResponse
     {
         $userDTO = $serializer->deserialize($request->getContent(), UserDTO::class, 'json');
-        $userManagement->updateUser($userDTO,$user,$customer);
+        $userManagement->updateUserOfCustomer($userDTO,$user,$customer);
 
         return $this->json('L\'utilisateur est mise à jour avec succès',200,['Content-Type' => 'text/plain']);
     }
@@ -75,9 +75,9 @@ class UserController extends AbstractController
      * @Entity("customer", expr="repository.getCustomer(customer_id)")
      * @Entity("user", expr="repository.getUser(user_id)")
      */
-    public function delete(User $user, UserManagement $userManagement, Customer $customer): JsonResponse
+    public function delete(User $user, UserOfCustomerManagement $userManagement, Customer $customer): JsonResponse
     {
-        $userManagement->deleteUser($user);
+        $userManagement->deleteUserOfCustomer($user);
 
         return $this->json('L\'utilisateur a été supprimé avec succès',200,['Content-Type' => 'text/plain']);
     }
