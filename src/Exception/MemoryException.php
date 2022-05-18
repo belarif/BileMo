207e70aed4cec3142a21631854d29e45100432aa
Belarif <b.ocine@live.fr>
2022-05-18 14:32:13 +0200

    BM50: creating new Exception in MemoryException.php and calling it from MemoryRepository

--- src/Exception/MemoryException.php
+++ src/Exception/MemoryException.php
@@ -6,8 +6,13 @@ use Exception;
 
 class MemoryException extends Exception
 {
-    public static function memoryExists(string $memoryCapacity)
+    public static function memoryExists(string $memoryCapacity): MemoryException
     {
         return new self("La mémoire de capacité $memoryCapacity est déjà existante !");
+    }
+
+    public static function notMemoryExists(): MemoryException
+    {
+        return new self("La capacité memoire demandé n'existe pas");
     }
 }
