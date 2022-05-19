1b07d0e8d2cc0bf44ba509b06696d432b58c7250
Belarif <b.ocine@live.fr>
2022-05-19 15:10:56 +0200

    BM50: make UserFixtures dependent on RoleFixtures when loading this one

--- src/DataFixtures/RoleFixtures.php
+++ src/DataFixtures/RoleFixtures.php
@@ -8,20 +8,29 @@ use Doctrine\Persistence\ObjectManager;
 
 class RoleFixtures extends Fixture
 {
+    public const ROLE_ADMIN = 'ROLE_ADMIN';
+    public const ROLE_CUSTOMER_ADMIN = 'ROLE_CUSTOMER_ADMIN';
+    public const ROLE_VISITOR = 'ROLE_VISIOR';
+
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
-        $roles = ['ROLE_ADMIN', 'ROLE_VISITOR'];
+        $roles = [
+            ['roleName' => self::ROLE_ADMIN],
+            ['roleName' => self::ROLE_CUSTOMER_ADMIN],
+            ['roleName' => self::ROLE_VISITOR]
+        ];
 
         foreach ($roles as $addRole) {
             $role = new Role();
 
-            $role->setRoleName($addRole);
+            $role->setRoleName($addRole['roleName']);
 
             $manager->persist($role);
             $manager->flush();
         }
     }
+
 }
