2b955f06b57b9245db99fc699f77255f12af9ee2
Belarif <b.ocine@live.fr>
2022-05-24 19:11:35 +0200

    BM49: creating documentation for APIs products management

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Exception\ProductException;
 use App\Repository\ProductRepository;
 use Exception;
+use OpenApi\Annotations as OA;
 use App\Entity\DTO\ProductDTO;
 use App\Service\ProductManagement;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
@@ -23,6 +24,67 @@ class ProductController extends AbstractController
 {
     /**
      * @Route("", name="create_product", methods={"POST"})
+     *
+     * @OA\Post(
+     *     path="/products",
+     *     summary="Create a new product",
+     *     tags="H",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name"
+     *             ),
+     *             @OA\Property(
+     *                 property="description"
+     *             ),
+     *             @OA\Property(
+     *                 property="color"
+     *             ),
+     *             @OA\Property(
+     *                 property="country",
+     *                 type="object"
+     *             ),
+     *             @OA\Property(
+     *                 property="brand",
+     *                 type="object"
+     *             ),
+     *             @OA\Property(
+     *                 property="memory",
+     *                 type="object"
+     *             ),
+     *             @OA\Property(
+     *                 property="user",
+     *                 type="object"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="HTTP_CREATED",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Created"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="409",
+     *         description="HTTP_CONFLICT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Conflict"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="HTTP_BAD_REQUEST",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Bad request"
+     *         )
+     *     )
+     * )
      */
     public function create(
         Request $request,
@@ -62,6 +124,21 @@ class ProductController extends AbstractController
 
     /**
      * @Route("", name="products_list", methods={"GET"})
+     *
+     * @OA\Get(
+     *     path="/products",
+     *     summary="Returns list of products",
+     *     tags="H",
+     *     @OA\Response(
+     *         response="200",
+     *         description="HTTP_OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/ProductDTO"),
+     *             description="Ok"
+     *         )
+     *     )
+     * )
      */
     public function list(ProductManagement $productManagement): JsonResponse
     {
@@ -69,7 +146,37 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="show_product", methods={"GET"})
+     * @Route("/{id}", name="show_product", methods={"GET"}, requirements={"id"="\d+"})
+     *
+     * @OA\Get(
+     *     path="/products/{id}",
+     *     summary="Returns product by id",
+     *     tags="H",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="product ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="HTTP_OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/ProductDTO"),
+     *             description="Ok"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function show(int $id, ProductRepository $productRepository): JsonResponse
     {
@@ -88,7 +195,84 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="update_product", methods={"PUT"})
+     * @Route("/{id}", name="update_product", methods={"PUT"}, requirements={"id"="\d+"})
+     *
+     * @OA\Put(
+     *     path="/products/{id}",
+     *     summary="Updates a product by id",
+     *     tags="H",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="product ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name"
+     *             ),
+     *             @OA\Property(
+     *                 property="description"
+     *             ),
+     *             @OA\Property(
+     *                 property="color"
+     *             ),
+     *             @OA\Property(
+     *                 property="country",
+     *                 type="object"
+     *             ),
+     *             @OA\Property(
+     *                 property="brand",
+     *                 type="object"
+     *             ),
+     *             @OA\Property(
+     *                 property="memory",
+     *                 type="object"
+     *             ),
+     *             @OA\Property(
+     *                 property="user",
+     *                 type="object"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="HTTP_CREATED",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/ProductDTO"),
+     *             description="Created"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="409",
+     *         description="HTTP_CONFLICT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Conflict"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="HTTP_BAD_REQUEST",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Bad request"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function update(
         int $id,
@@ -134,14 +318,42 @@ class ProductController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="delete_product", methods={"DELETE"})
+     * @Route("/{id}", name="delete_product", methods={"DELETE"}, requirements={"id"="\d+"})
+     *
+     * @OA\Delete(
+     *     path="/products/{id}",
+     *     summary="Deletes a product by id",
+     *     tags="H",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="product ID",
+     *         required=true
+     *     ),
+     *     @OA\Response(
+     *         response="204",
+     *         description="HTTP_NO_CONTENT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="No content"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function delete(int $id, ProductRepository $productRepository, ProductManagement $productManagement): JsonResponse
     {
         try {
             $productManagement->deleteProduct($productRepository->getProduct($id));
 
-            return $this->json('Le produit est supprimé avec succès', Response::HTTP_OK);
+            return $this->json('Le produit est supprimé avec succès', Response::HTTP_NO_CONTENT);
 
         } catch (ProductException $e) {
             return $this->json(
