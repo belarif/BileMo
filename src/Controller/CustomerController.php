e3cd7403a91e1c4fa62a34bc5eb407a30f776b6e
Belarif <b.ocine@live.fr>
2022-05-24 16:15:34 +0200

    BM49: creating documentation for APIs customer management

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Exception\CustomerException;
 use Exception;
+use OpenApi\Annotations as OA;
 use App\Entity\DTO\CustomerDTO;
 use App\Repository\CustomerRepository;
 use App\Service\CustomerManagement;
@@ -23,6 +24,49 @@ class CustomerController extends AbstractController
 {
     /**
      * @Route("", name="create_customer", methods={"POST"})
+     *
+     * @OA\Post(
+     *     path="/customers",
+     *     summary="Create a new customer",
+     *     tags="E",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="company"
+     *             ),
+     *             @OA\Property(
+     *                 property="enabled",
+     *                 type="boolean"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="HTTP_CREATED",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Created"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="409",
+     *         description="HTTP_CONFLICT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Conflict"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="HTTP_BAD_REQUEST",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Bad request"
+     *         )
+     *     )
+     * )
      */
     public function create(
         Request $request,
@@ -59,6 +103,21 @@ class CustomerController extends AbstractController
 
     /**
      * @Route("", name="customers_list", methods={"GET"})
+     *
+     * @OA\Get(
+     *     path="/customers",
+     *     summary="Returns list of customers",
+     *     tags="E",
+     *     @OA\Response(
+     *         response="200",
+     *         description="HTTP_OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CustomerDTO"),
+     *             description="Ok"
+     *         )
+     *     )
+     * )
      */
     public function list(CustomerManagement $customerManagement): JsonResponse
     {
@@ -67,6 +126,36 @@ class CustomerController extends AbstractController
 
     /**
      * @Route("/{customer_id}", name="show_customer", methods={"GET"})
+     *
+     * @OA\Get(
+     *     path="/customers/{id}",
+     *     summary="Returns customer by id",
+     *     tags="E",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="customer ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="HTTP_OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CustomerDTO"),
+     *             description="Ok"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function show(int $customer_id, CustomerRepository $customerRepository): JsonResponse
     {
@@ -86,6 +175,64 @@ class CustomerController extends AbstractController
 
     /**
      * @Route("/{customer_id}", name="update_customer", methods={"PUT"})
+     *
+     * @OA\Put(
+     *     path="/customers/{id}",
+     *     summary="Updates a customer by id",
+     *     tags="E",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="customer ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="company"
+     *             ),
+     *             @OA\Property(
+     *                 property="enabled"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="HTTP_CREATED",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CustomerDTO"),
+     *             description="Created"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="409",
+     *         description="HTTP_CONFLICT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Conflict"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="HTTP_BAD_REQUEST",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Bad request"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function update(
         int $customer_id,
@@ -137,13 +284,41 @@ class CustomerController extends AbstractController
 
     /**
      * @Route("/{customer_id}", name="delete_customer", methods={"DELETE"})
+     *
+     * @OA\Delete(
+     *     path="/customers/{id}",
+     *     summary="Deletes a customer by id",
+     *     tags="E",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="customer ID",
+     *         required=true
+     *     ),
+     *     @OA\Response(
+     *         response="204",
+     *         description="HTTP_NO_CONTENT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="No content"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function delete(int $customer_id, CustomerRepository $customerRepository, CustomerManagement $customerManagement): JsonResponse
     {
         try {
             $customerManagement->deletecCustomer($customerRepository->getCustomer($customer_id));
 
-            return $this->json('Le client est supprimé avec succès', Response::HTTP_OK);
+            return $this->json('Le client est supprimé avec succès', Response::HTTP_NO_CONTENT);
 
         } catch (CustomerException $e) {
             return $this->json(
--- src/Entity/DTO/CustomerDTO.php
+++ src/Entity/DTO/CustomerDTO.php
@@ -2,19 +2,40 @@
 
 namespace App\Entity\DTO;
 
+use App\Entity\User;
 use Symfony\Component\Validator\Constraints as Assert;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Schema()
+ */
 class CustomerDTO
 {
+    /**
+     * @OA\Property(type="integer")
+     */
     public int $id;
 
+    /**
+     * @OA\Property(type="string")
+     */
     public string $code;
 
+    /**
+     * @OA\Property(type="boolean")
+     */
     public $enabled;
 
     /**
      * @Assert\NotBlank(message="Le champs société ne peut pas être vide")
      * @Assert\Length(max=60, maxMessage="Le champs société ne doit pas dépasser {{ limit }} caractères")
+     *
+     * @OA\Property(type="string")
      */
     public string $company;
+
+    /**
+     * @OA\Property(type="array")
+     */
+    public user $users;
 }
