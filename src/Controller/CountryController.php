da3095a9442e4d06e44062e79e6d7e6348e70b7e
Belarif <b.ocine@live.fr>
2022-05-24 15:34:53 +0200

    BM49: creating documentation for APIs country management

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controller;
 
+use Exception;
+use OpenApi\Annotations as OA;
 use App\Entity\DTO\CountryDTO;
 use App\Exception\CountryException;
 use App\Repository\CountryRepository;
 use App\Service\CountryManagement;
-use Exception;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -23,6 +24,45 @@ class CountryController extends AbstractController
 {
     /**
      * @Route("", name="create_country", methods={"POST"})
+     *
+     * @OA\Post(
+     *     path="/countries",
+     *     summary="Create a new country",
+     *     tags="C",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="HTTP_CREATED",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Created"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="409",
+     *         description="HTTP_CONFLICT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Conflict"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="HTTP_BAD_REQUEST",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Bad request"
+     *         )
+     *     )
+     * )
      */
     public function create(
         Request $request,
@@ -59,6 +99,21 @@ class CountryController extends AbstractController
 
     /**
      * @Route("", name="countries_list", methods={"GET"})
+     *
+     * @OA\Get(
+     *     path="/countries",
+     *     summary="Returns list of countries",
+     *     tags="C",
+     *     @OA\Response(
+     *         response="200",
+     *         description="HTTP_OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CountryDTO"),
+     *             description="Ok"
+     *         )
+     *     )
+     * )
      */
     public function list(CountryManagement $countryManagement): JsonResponse
     {
@@ -67,6 +122,36 @@ class CountryController extends AbstractController
 
     /**
      * @Route("/{id}", name="show_country", methods={"GET"}, requirements={"id"="\d+"})
+     *
+     * @OA\Get(
+     *     path="/countries/{id}",
+     *     summary="Returns country by id",
+     *     tags="C",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="country ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="HTTP_OK",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CountryDTO"),
+     *             description="Ok"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function show(int $id, CountryRepository $countryRepository): JsonResponse
     {
@@ -86,6 +171,61 @@ class CountryController extends AbstractController
 
     /**
      * @Route("/{id}", name="update_country", methods={"PUT"}, requirements={"id"="\d+"})
+     *
+     * @OA\Put(
+     *     path="/countries/{id}",
+     *     summary="Updates a country by id",
+     *     tags="C",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="country ID",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="name"
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="201",
+     *         description="HTTP_CREATED",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/CountryDTO"),
+     *             description="Created"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="409",
+     *         description="HTTP_CONFLICT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Conflict"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="400",
+     *         description="HTTP_BAD_REQUEST",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Bad request"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function update(
         int $id,
@@ -132,13 +272,41 @@ class CountryController extends AbstractController
 
     /**
      * @Route("/{id}", name="delete_country", methods={"DELETE"}, requirements={"id"="\d+"})
+     *
+     * @OA\Delete(
+     *     path="/countries/{id}",
+     *     summary="Deletes a country by id",
+     *     tags="C",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="country ID",
+     *         required=true
+     *     ),
+     *     @OA\Response(
+     *         response="204",
+     *         description="HTTP_NO_CONTENT",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="No content"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="HTTP_NOT_FOUND",
+     *         @OA\JsonContent(
+     *             type="string",
+     *             description="Not found"
+     *         )
+     *     )
+     * )
      */
     public function delete(int $id, CountryRepository $countryRepository, CountryManagement $countryManagement): JsonResponse
     {
         try {
             $countryManagement->deleteCountry($countryRepository->getCountry($id));
 
-            return $this->json('La pays a été supprimé avec succès', Response::HTTP_OK);
+            return $this->json('La pays a été supprimé avec succès', Response::HTTP_NO_CONTENT);
 
         } catch (CountryException $e) {
             return $this->json(
