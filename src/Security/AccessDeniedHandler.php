925b02c00d1f2ac93a4cdece1766b39565317b90
Belarif <b.ocine@live.fr>
2022-06-02 16:48:21 +0200

    BM52: customizing the Forbidden Response

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler extends AbstractController implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): JsonResponse
+    {
+        return $this->json(
+            [
+                "success" => "false",
+                "message" => "Vous n'etes pas autorisé à accéder à cette page"
+            ]
+            ,Response::HTTP_FORBIDDEN
+        );
+    }
+}
